c6642b11435159a7636586525e63d9c919b33db6
Alaidar Bakdaulet <bakdauletcoutinho@gmail.com>
2023-02-16 21:29:41 +0600

    PRO

--- lab3/Task2/todolist.js
+++ lab3/Task2/todolist.js
@@ -1,50 +1,42 @@
-var myList = document.getElementsByTagName('LI');
-for(var i = 0; i < myList.length; i++){
-    var span = document.createElement("span");
-    var txt = document.createTextNode("\u00D7");
-    span.className = 'close';
-    span.appendChild(txt);
-    myList[i].appendChild(span);
+const input = document.querySelector('#input');
+const btn = document.querySelector('#btn');
+const result = document.querySelector('#result');
+
+
+
+// add event
+btn.addEventListener('click', (e) => {
+    if(input.value === '') return;
+    createDeleteElement(input.value);
+    input.value = '';
+})
+
+
+// create and delete
+function createDeleteElement(value) {
+    console.log(value);
+
+    const li = document.createElement('li');
+    const btn = document.createElement('btn');
+
+    li.className = 'li';
+    li.textContent = value;
+
+    btn.className = 'btn';
+    btn.textContent = 'X';
+
+    li.appendChild(btn); 
+
+    // remove item
+    btn.addEventListener('click', (e) => {
+        console.log(li);
+
+        result.removeChild(li);
+    })   
+
+    li.addEventListener('click', (e) => {
+        li.classList.toggle('li-active');
+    })
+
+    result.appendChild(li);
 }
-
-var close = document.getElementsByClassName('close');
-for(var i = 0; i < close.length; i++){
-    close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-    }
-}
-
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-
-
-function newElement() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-      alert("You must write something!");
-    } else {
-      document.getElementById("myUL").appendChild(li);
-    }
-    document.getElementById("myInput").value = "";
-  
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-  
-    for (i = 0; i < close.length; i++) {
-      close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-      }
-    }
-  }
